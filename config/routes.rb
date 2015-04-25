872418242831777efa684d69469330317ee249b4
Sylvain <sylvain@sylvain-Inspiron-7520.local>
2015-04-25 21:59:00 +0200

    nouveau controller users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+
+  match '/signup', to: 'users#new', via: :get
+
   match '/contact', to: 'pages#contact',via: :get
   match '/about',   to:'pages#about',via: :get
   match '/help',    to: 'pages#help',via: :get
