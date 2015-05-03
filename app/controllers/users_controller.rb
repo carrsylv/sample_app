f57a17dd5c609347a8cfebf3d5d445df695874b0
Sylvain <sylvain@sylvain-Inspiron-7520.local>
2015-05-03 19:00:30 +0200

    Inscription utilisateur fini

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,23 @@ class UsersController < ApplicationController
   end
 
   def new
-    @titre = "S'inscrire"
+    @user = User.new
+    @titre = "Inscription"
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Bienvenue dans l'Application Exemple!"
+      redirect_to @user
+    else
+      @titre = "Inscription"
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:nom, :email, :password, :salt, :encrypted_password)
+  end
+
 end
